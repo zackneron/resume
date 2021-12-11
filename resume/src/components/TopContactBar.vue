73b7ecb5dd84ca82f8110220bff306d85e365518
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2021-12-11 17:58:51 +0200

    Top contact bar comp

---
+++ resume/src/components/TopContactBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="media">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="media__info">
+                        <a href="mailto:zackneron@gmail.com">zackneron@gmail.com</a>
+                        <a href="tel:+27624129173">0624129173</a>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <a href="">
+                        <i class="fa fa-info"></i>
+                    </a>
+                        <a href="tel:+27624129173">0624129173</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopContact',
+}
+</script>
